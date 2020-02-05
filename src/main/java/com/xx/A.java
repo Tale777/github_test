f9105ebaff6aa286db8ca5b50243b0f8ee18fb0d
litianchu <litianchu@sina.com>
2020-02-05 17:14:44 +0800

    test项目推送1

--- src/main/java/com/xx/A.java
+++ src/main/java/com/xx/A.java
@@ -3,6 +3,6 @@ package com.xx;
 public class A {
     public static void main(String[] args) {
         System.out.println("this is a test class A");
-        System.out.println(12);
+        System.out.println(122312);
     }
 }
